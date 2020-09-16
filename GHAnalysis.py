bf86f951c618e8846e49621e6ceef93deb298b59
gaotimin <693960221@qq.com>
2020-09-16 23:51:57 +0800

    code modification

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -81,5 +81,23 @@ def run():
     my_parser.add_argument('-r', '--repo', help='repository name')
     my_parser.add_argument('-e', '--event', help='type of event')
     args = my_parser.parse_args()
+
+    if args.init:
+        my_data = Data(path=args.init)
+    else:
+        my_data =  Data()
+        if args.event:
+            if args.user:
+                if args.repo:
+                    print(my_data.query_cnt_user_and_repo(args.user, args.repo, args.event))
+                else:
+                    print(my_data.query_cnt_user(args.user, args.event))
+            else:
+                if args.repo:
+                    print(my_data.query_cnt_repo(args.repo, args.event))
+                else:
+                    print("missing argument: user or repo")
+        else:
+            print("missing argument: event")
 if __name__ == '__main__':
     run()
