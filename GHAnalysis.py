8e7a9bb4c7a93c56494ba5ce6552ed3b48be3153
gaotimin <693960221@qq.com>
2020-09-16 10:40:31 +0800

    code modification

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -28,6 +28,11 @@ class Data:
 
 
 def run():
-
+    my_parser = argparse.ArgumentParser(description='analysis the json file')
+    my_parser.add_argument('-i', '--init', help='json file path')
+    my_parser.add_argument('-u', '--user', help='username')
+    my_parser.add_argument('-r', '--repo', help='repository name')
+    my_parser.add_argument('-e', '--event', help='type of event')
+    args = my_parser.parse_args()
 if __name__ == '__main__':
     run()
